e91c311774fcf911a6cd0a7dd0a7d63606da7fd2
mt <triminhphan11800@gmail.com>
2023-10-29 18:56:21 +0700

    create ui menu app

---
+++ src/components/AppBar/Menus/Profiles.jsx
@@ -0,0 +1,94 @@
+import React from 'react'
+import Button from '@mui/material/Button'
+import Menu from '@mui/material/Menu'
+import MenuItem from '@mui/material/MenuItem'
+import Box from '@mui/material/Box'
+import Divider from '@mui/material/Divider'
+import ListItemText from '@mui/material/ListItemText'
+import ListItemIcon from '@mui/material/ListItemIcon'
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore'
+import ExpandLessIcon from '@mui/icons-material/ExpandLess'
+import Check from '@mui/icons-material/Check'
+import Avatar from '@mui/material/Avatar'
+import IconButton from '@mui/material/IconButton'
+import Typography from '@mui/material/Typography'
+import Tooltip from '@mui/material/Tooltip'
+import PersonAdd from '@mui/icons-material/PersonAdd'
+import Settings from '@mui/icons-material/Settings'
+import Logout from '@mui/icons-material/Logout'
+
+function Profiles() {
+  const [anchorEl, setAnchorEl] = React.useState(null)
+  const open = Boolean(anchorEl)
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget)
+  }
+  const handleClose = () => {
+    setAnchorEl(null)
+  }
+
+  return (
+    <Box>
+      <Tooltip title="Account settings">
+        <IconButton
+          onClick={handleClick}
+          size="small"
+          sx={{ padding: 0 }}
+          aria-controls={open ? 'account-menu' : undefined}
+          aria-haspopup="true"
+          aria-expanded={open ? 'true' : undefined}
+        >
+          <Avatar
+            sx={{ width: 32, height: 32 }}
+            src="https://cdn.dribbble.com/userupload/3828718/file/original-fe81a0231432c19ee76c834ed7d81ca7.png?resize=752x"
+          />
+        </IconButton>
+      </Tooltip>
+      <Menu
+        id="basic-menu-profiles"
+        anchorEl={anchorEl}
+        open={open}
+        onClose={handleClose}
+        MenuListProps={{
+          'aria-labelledby': 'basic-button-profiles'
+        }}
+      >
+        <MenuItem onClick={handleClose}>
+          <Avatar
+            sx={{ width: 28, height: 28, mr: 2 }}
+            src="https://cdn.dribbble.com/userupload/3828718/file/original-fe81a0231432c19ee76c834ed7d81ca7.png?resize=752x"
+          />{' '}
+          Profile
+        </MenuItem>
+        <MenuItem onClick={handleClose}>
+          <Avatar
+            sx={{ width: 28, height: 28, mr: 2 }}
+            src="https://cdn.dribbble.com/userupload/3828718/file/original-fe81a0231432c19ee76c834ed7d81ca7.png?resize=752x"
+          />{' '}
+          My account
+        </MenuItem>
+        <Divider />
+        <MenuItem onClick={handleClose}>
+          <ListItemIcon>
+            <PersonAdd fontSize="small" />
+          </ListItemIcon>
+          Add another account
+        </MenuItem>
+        <MenuItem onClick={handleClose}>
+          <ListItemIcon>
+            <Settings fontSize="small" />
+          </ListItemIcon>
+          Settings
+        </MenuItem>
+        <MenuItem onClick={handleClose}>
+          <ListItemIcon>
+            <Logout fontSize="small" />
+          </ListItemIcon>
+          Logout
+        </MenuItem>
+      </Menu>
+    </Box>
+  )
+}
+
+export default Profiles
